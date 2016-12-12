53b31d404ee60cee7f1d9b59a159ff20930bdf6d
Ludovic Delauné <ludovic.delaune@oslandia.com>
2016-12-12 17:50:11 +0100

    change flask and psycopg2 from fixed to minimal version

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ import subprocess
 GDAL_VERSION = subprocess.check_output(['gdal-config', '--version']).strip().decode()
 GDAL_VERSION, GDAL_REVISION = GDAL_VERSION[:GDAL_VERSION.rfind('.')].split('.')
 GDAL_MIN = '{0}.{1}'.format(GDAL_VERSION, GDAL_REVISION)
-GDAL_MAX = '{0}.{1}'.format(GDAL_VERSION, int(GDAL_REVISION)+1)
+GDAL_MAX = '{0}.{1}'.format(GDAL_VERSION, int(GDAL_REVISION) + 1)
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 requirements = (
-    'flask==0.10.1',
+    'flask>=0.10.1',
     'flask-restplus==0.9.2',
-    'psycopg2==2.6.1',
+    'psycopg2>=2.6.1',
     'pyyaml',
     'pygdal >= {0}, <{1}'.format(GDAL_MIN, GDAL_MAX),
     'redis',
