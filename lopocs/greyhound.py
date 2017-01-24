e24a456c61c3a133d0bf84ebd3ff63fb868301a9
Éric Lemoine <eric.lemoine@oslandia.com>
2017-01-24 15:24:55 +0100

    Change pc_patchtransform to pc_setpcid

--- lopocs/greyhound.py
+++ lopocs/greyhound.py
@@ -186,7 +186,7 @@ def sql_query(box, schema_pcid, lod):
         sql_limit = " limit {0} ".format(Config.MAX_PATCHS_PER_QUERY)
 
     if Config.USE_MORTON:
-        sql = ("select pc_compress(pc_patchtransform(pc_union("
+        sql = ("select pc_compress(pc_setpcid(pc_union("
                "pc_filterbetween( "
                "pc_range({0}, {4}, {5}), 'Z', {6}, {7} )), {9}), 'laz') from "
                "(select {0} from {1} "
@@ -197,7 +197,7 @@ def sql_query(box, schema_pcid, lod):
                        box[2], box[5], sql_limit,
                        schema_pcid))
     else:
-        sql = ("select pc_compress(pc_patchtransform(pc_union("
+        sql = ("select pc_compress(pc_setpcid(pc_union("
                "pc_filterbetween( "
                "pc_range({0}, {4}, {5}), 'Z', {6}, {7} )), {9}), 'laz') from "
                "(select {0} from {1} where pc_intersects({0}, "
